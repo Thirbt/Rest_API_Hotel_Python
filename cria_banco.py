e0fbd0394dd6ac8ef82a0ae93ee2759eec97010a
Thirbt <thiago.godo@hotmail.com>
2024-08-29 16:56:11 -0300

    Adicionando o INSERT para adicionar um hotel dentro do banco de dados

--- cria_banco.py
+++ cria_banco.py
@@ -7,7 +7,10 @@ cursor = connection.cursor()
 cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY,\
     nome text, estrelas real, valorDiaria real, cidade text)"
     
+cria_hotel = "INSERT INTO hoteis VALUES ('alpha', 'Alpha hotel', 4.3, 420.34, 'Rio de Janeiro')"
+    
 cursor.execute(cria_tabela)
+cursor.execute(cria_hotel)
 
 connection.commit()
 connection.close()
